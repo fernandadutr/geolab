8553690bb04cbb31c3a8eee3958292e92d9fa3c2
Patrick <patrickz.t@outlook.com>
2023-09-28 16:39:29 -0300

    Adiciona botão de obter determinação

--- Componentes/TeorUmidade/Passo5.tsx
+++ Componentes/TeorUmidade/Passo5.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 
 interface Passo5Props {
@@ -20,15 +20,12 @@ function calcularW(massa1: number, massa2: number, massa3: number): number | nul
     return wArredondado;
 }
 
-function calcularMedia(valor1: number, valor2: number, valor3: number): number {
-    const soma = valor1 + valor2 + valor3;
-    const media = soma / 3;
-    return media;
-}
-
 const Passo5: React.FC<Passo5Props> = ({ dadosPasso1, dadosPasso2, dadosPasso4 }) => {
     const [isChecked1, setIsChecked1] = useState(false);
     const [isChecked2, setIsChecked2] = useState(false);
+    const [resultado1, setResultado1] = useState<number | null>(null);
+    const [resultado2, setResultado2] = useState<number | null>(null);
+    const [resultado3, setResultado3] = useState<number | null>(null);
 
     const handleCheckboxToggle1 = () => {
         setIsChecked1(!isChecked1);
@@ -36,6 +33,18 @@ const Passo5: React.FC<Passo5Props> = ({ dadosPasso1, dadosPasso2, dadosPasso4 }
 
     const handleCheckboxToggle2 = () => {
         setIsChecked2(!isChecked2);
+    };
+
+    const calcularDeterminacoes = () => {
+        // Calcular o valor de w para cada determinação
+        const w1 = calcularW(parseFloat(dadosPasso1.massa1), parseFloat(dadosPasso1.massa2), parseFloat(dadosPasso1.massa3));
+        const w2 = calcularW(parseFloat(dadosPasso2.massa1), parseFloat(dadosPasso2.massa2), parseFloat(dadosPasso2.massa3));
+        const w3 = calcularW(parseFloat(dadosPasso4.massa1), parseFloat(dadosPasso4.massa2), parseFloat(dadosPasso4.massa3));
+
+        // Atualizar os estados com os resultados
+        setResultado1(w1);
+        setResultado2(w2);
+        setResultado3(w3);
     };
 
     return (
@@ -65,16 +74,13 @@ const Passo5: React.FC<Passo5Props> = ({ dadosPasso1, dadosPasso2, dadosPasso4 }
             </Text>
             <View style={{ alignItems: 'center' }}>
                 <Text style={{ color: '#5F6811' }}>
-                    1ª Determinação: Teor de Umidade (w) ={' '}
-                    {calcularW(parseFloat(dadosPasso1.massa1), parseFloat(dadosPasso1.massa2), parseFloat(dadosPasso1.massa3))}
+                    1ª Determinação: Teor de Umidade (w) = {resultado1 !== null ? resultado1 : 'Aguardando cálculo'}
                 </Text>
                 <Text style={{ color: '#5F6811' }}>
-                    2ª Determinação: Teor de Umidade (w) ={' '}
-                    {calcularW(parseFloat(dadosPasso2.massa1), parseFloat(dadosPasso2.massa2), parseFloat(dadosPasso2.massa3))}
+                    2ª Determinação: Teor de Umidade (w) = {resultado2 !== null ? resultado2 : 'Aguardando cálculo'}
                 </Text>
                 <Text style={{ color: '#5F6811' }}>
-                    3ª Determinação: Teor de Umidade (w) ={' '}
-                    {calcularW(parseFloat(dadosPasso4.massa1), parseFloat(dadosPasso4.massa2), parseFloat(dadosPasso4.massa3))}
+                    3ª Determinação: Teor de Umidade (w) = {resultado3 !== null ? resultado3 : 'Aguardando cálculo'}
                 </Text>
             </View>
 
@@ -85,6 +91,8 @@ const Passo5: React.FC<Passo5Props> = ({ dadosPasso1, dadosPasso2, dadosPasso4 }
                 checkedColor="#A8B444"
                 containerStyle={{ borderRadius: 9 }}
             />
+
+            <Button title="Obter Determinações" onPress={calcularDeterminacoes} />
         </View>
     );
 };
