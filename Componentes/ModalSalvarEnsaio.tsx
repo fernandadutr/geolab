8fa952d1b805d18e9340abb6da56a9ad880f8a6f
Patrick <patrickz.t@outlook.com>
2023-10-11 16:10:46 -0300

    Adiciona opção de compartilhamento

--- Componentes/ModalSalvarEnsaio.tsx
+++ Componentes/ModalSalvarEnsaio.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Share } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
@@ -8,6 +8,7 @@ import { useUser } from '../Context/Context';
 import { useEnsaios } from '../Context/EnsaiosContext';
 import styles from '../Styles/Componentes';
 import Modal from 'react-native-modal';
+import { Image } from 'react-native-elements';
 
 interface ModalSalvarEnsaioProps {
     visible: boolean;
@@ -40,6 +41,24 @@ const ModalSalvarEnsaio: React.FC<ModalSalvarEnsaioProps> = ({ visible, onClose 
         navigation.navigate('Home');
     };
 
+    const handleCompartilharEnsaio = () => {
+        const textoCompartilhamento = `Nome do Ensaio: ${nomeEnsaio}\nData: ${new Date()}\nConcluídos: Teor de Umidade`;
+
+        Share.share({
+            message: textoCompartilhamento,
+            title: 'Compartilhar Ensaio',
+        })
+            .then(result => {
+                if (result.action === Share.sharedAction) {
+                    console.log('Mensagem compartilhada com sucesso.');
+                } else if (result.action === Share.dismissedAction) {
+                    console.log('Compartilhamento cancelado');
+                }
+            })
+            .catch(error => console.error(error));
+    };
+
+
     return (
         <Modal
             isVisible={visible}
@@ -58,17 +77,33 @@ const ModalSalvarEnsaio: React.FC<ModalSalvarEnsaioProps> = ({ visible, onClose 
                     <TextInput
                         value={nomeEnsaio}
                         onChangeText={setNomeEnsaio}
-                        style={{backgroundColor: '#F1F5F4', borderRadius: 9}}
+                        style={{ backgroundColor: '#F1F5F4', borderRadius: 9 }}
                         placeholder="Digite o nome do ensaio"
                     />
-                    <TouchableOpacity
-                        style={styles.ensaioContainer}
-                        onPress={handleSalvarEnsaio}
-                    >
-                        <Text style={{ marginVertical: 5, fontWeight: 'bold' }}>
-                            Salvar Ensaio
-                        </Text>
-                    </TouchableOpacity>
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between', // Controla o espaço entre os botões
+                        width: '50%',
+                    }}>
+
+                        <TouchableOpacity
+                            style={{ backgroundColor: '#A8B444', borderRadius: 9, margin: 10, padding: 10 }}
+                            onPress={handleSalvarEnsaio}
+                        >
+                            <Text style={{ marginVertical: 5, fontWeight: 'bold' }}>
+                                Salvar Ensaio
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{ backgroundColor: '#A8B444', borderRadius: 9, margin: 10, padding: 10 }}
+                            onPress={handleCompartilharEnsaio}
+                        >
+                            <Image source={require('./TeorUmidade/sharePNG.png')}
+                                style={{ width: 22, height: 25}}
+                            />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </Modal>
